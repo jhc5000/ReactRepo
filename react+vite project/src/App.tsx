efb878ecf6b755ae25336fd848238e95534cbe3b
John Carter V <34918265+jhc5000@users.noreply.github.com>
2026-01-21 13:11:59 +0000

    added some logic and text

--- react+vite project/src/App.tsx
+++ react+vite project/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>JOHN'S Vite + React PROJECT</h1>
       <div className="card">
         <button onClick={() => setCount((count) => (count + 1) * 2)}>
           count is {count}
