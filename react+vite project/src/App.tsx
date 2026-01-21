0fdfc7afdc8683ae1ee08a101d4f36786fcf2a3c
John Carter V <34918265+jhc5000@users.noreply.github.com>
2026-01-21 13:15:53 +0000

    update commit msg and some app text

--- react+vite project/src/App.tsx
+++ react+vite project/src/App.tsx
@@ -24,6 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p>Next we're gonna try importing some dashboard UI/UX components.</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
