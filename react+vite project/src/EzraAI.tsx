ed96429626926cd4915470be18cb7eea36c4cd55
John Carter V <34918265+jhc5000@users.noreply.github.com>
2026-02-21 06:16:03 +0000

    abstract the ezra-ai component

---
+++ react+vite project/src/EzraAI.tsx
@@ -0,0 +1,18 @@
+export default function EzraAI() {
+  return (
+    <div id="EzraAI" className="place-items-center">
+      <div
+        className="ezraAI-wrapper mt-15 bg-white dark:bg-neutral-700 rounded-lg rounded-md rounded-sm rounded-xl"
+        style={{ width: "30vw", height: "30vh", backgroundColor: "lightcoral" }}
+      >
+        <h1>EZRA.AI</h1>
+        <div
+          className="ezraAI-content mt-5 rounded-lg rounded-md rounded-sm rounded-xl"
+          // style={{ backgroundColor: "lightcoral" }}
+        >
+          <p>placeholder</p>
+        </div>
+      </div>
+    </div>
+  );
+}
